7129bd4cff13cbefb66bce54b720e595e0297dce
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-06 02:23:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,7 +49,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
     end
     
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
   
   
