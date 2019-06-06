9f971b0d59f6b52a4729576e9bdf9252f822481a
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-06 03:12:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -91,7 +91,18 @@ class Dog
   end
 
   # database to ruby
-  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
   
   
